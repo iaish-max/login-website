4ed91851fabc0e81297ff3cccc077f7f5a55e22b
iaish-max <aishwarybhadauriya09@gmail.com>
2021-06-03 02:08:22 +0530

    add env var

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const ejs = require("ejs");
@@ -17,9 +18,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "prem naam hai mera, prem chopra.";
 
-userSchema.plugin(encrypt, {secret: secret , encryptedFields: ['password'] });
+userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = mongoose.model("User",userSchema);
 
